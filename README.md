9740073dd16ea35687a72c29490c0e5912f18998
Kevin Choubacha <chewbacha@gmail.com>
2018-06-10 13:23:03 -0700

    Use a large array of bytes instead of the same bytes

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # Benchmark str radix vs char radix
 
+
+### debug
 ```
-            from str: Duration { secs: 1, nanos: 176672462 }
-           from char: Duration { secs: 0, nanos: 40 }
-    no str formation: Duration { secs: 0, nanos: 560581600 }
-       str formation: Duration { secs: 0, nanos: 711160985 }
+$ cargo run
+   Compiling hex-escape-bench v0.1.0 (file:///Users/kchoubacha/repos/kbacha/hex-escape-bench)
+    Finished dev [unoptimized + debuginfo] target(s) in 0.47 secs
+     Running `target/debug/hex-escape-bench`
+            from str: Duration { secs: 5, nanos: 108206998 }
+           from char: Duration { secs: 7, nanos: 827446431 }
+       str formation: Duration { secs: 3, nanos: 791812058 }
+```
+
+### release
+```
+$ cargo run --release
+   Compiling hex-escape-bench v0.1.0 (file:///Users/kchoubacha/repos/kbacha/hex-escape-bench)
+    Finished release [optimized] target(s) in 0.37 secs
+     Running `target/release/hex-escape-bench`
+            from str: Duration { secs: 1, nanos: 201551561 }
+           from char: Duration { secs: 0, nanos: 105942603 }
+       str formation: Duration { secs: 0, nanos: 684496518 }
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use std::time::Instant;
 
-/// Executes a closure once and returns how long that closure took
-/// as a duration.
 pub fn time_it(name: &str, f: impl FnOnce()) {
     let start = Instant::now();
     f();
@@ -10,6 +8,20 @@ pub fn time_it(name: &str, f: impl FnOnce()) {
 
 fn main() {
     let iters = 100_000_000;
+    // Build a byte array of valid hex characters. This way the results are deterministic
+    // and not optimized out by the compiler.
+    let mut bytes: Vec<(u8, u8)> = Vec::with_capacity(iters);
+
+    for i in 0..iters {
+        bytes.push(
+            (
+                b'0' + (i % 10) as u8,
+                b'a' + (i % 6) as u8
+            )
+        );
+    }
+
+    // Just make sure that they are the same
     assert_eq!(
         u8::from_str_radix(std::str::from_utf8(&[97u8, 98u8]).unwrap(), 16).unwrap(),
         (
@@ -19,29 +31,23 @@ fn main() {
     );
 
     time_it("from str", || {
-        for _ in 0..iters {
-            u8::from_str_radix(std::str::from_utf8(&[97u8, 98u8]).unwrap(), 16).unwrap();
+        for (b1, b2) in bytes.iter() {
+            u8::from_str_radix(std::str::from_utf8(&[*b1, *b2]).unwrap(), 16).unwrap();
         }
     });
 
     time_it("from char", || {
-        for _ in 0..iters {
+        for (b1, b2) in bytes.iter() {
             (
-                (char::from(97u8).to_digit(16).unwrap() << 4) +
-                 char::from(98u8).to_digit(16).unwrap()
+                (char::from(*b1).to_digit(16).unwrap() << 4) +
+                 char::from(*b2).to_digit(16).unwrap()
             ) as u8;
         }
     });
 
-    time_it("no str formation", || {
-        for _ in 0..iters {
-            u8::from_str_radix(&"ab", 16).unwrap();
-        }
-    });
-
     time_it("str formation", || {
-        for _ in 0..iters {
-            std::str::from_utf8(&[97u8, 98u8]).unwrap();
+        for (b1, b2) in bytes.iter() {
+            std::str::from_utf8(&[*b1, *b2]).unwrap();
         }
     });
 }
